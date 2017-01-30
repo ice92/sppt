3aa7baa0a9c626c87416dd2f7a31320833864df8
Imam Arief Putrajaya <ice.codename@gmail.com>
2017-01-30 12:59:40 +0800

    logo web di toolbar & user guide
    - Ganti logo di toolbar
    - hapus folder user guide

--- application/views/template/Template.php
+++ application/views/template/Template.php
@@ -5,7 +5,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <LINK REL="SHORTCUT ICON" href="<?php echo base_url('assets/img/lobar mini.jpg') ?>">
     <title>SPPT | Sistem Pelayanan Perizinan Terpadu</title>
 
     <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
